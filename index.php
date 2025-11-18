e0339b3c49786352adf921e91ba9dfa2f509ac2f
Lecrot Gaulier Romain <73986902+healthyromain@users.noreply.github.com>
2025-11-18 08:33:21 +0100

    Découvrez les méthodes particulières

--- index.php
+++ index.php
@@ -5,10 +5,24 @@ declare(strict_types=1);
 class Player
 {
     /** @var int */
-    public $level = 0;
+    private $level = 0;
 
     public function __construct(int $level = 0)
     {
+        $this->setLevel($level);
+    }
+
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level)
+    {
+        if ($level < 0) {
+            trigger_error('Le niveau doit être positif ou nul.', E_USER_ERROR);
+        }
+
         $this->level = $level;
     }
 }
@@ -32,7 +46,9 @@ class Encounter
             return;
         }
 
-        $playerOne->level += (int) (32 * ($playerOneResult - self::probabilityAgainst($playerOne->level, $againstLevelPlayerTwo)));
+        $current = $playerOne->getLevel();
+        $delta = (int) (32 * ($playerOneResult - self::probabilityAgainst($current, $againstLevelPlayerTwo)));
+        $playerOne->setLevel($current + $delta);
     }
 }
 
@@ -42,17 +58,17 @@ $jade = new Player(800);
 
 echo sprintf(
     'Greg à %.2f%% chance de gagner face a Jade',
-    Encounter::probabilityAgainst($greg->level, $jade->level) * 100
+    Encounter::probabilityAgainst($greg->getLevel(), $jade->getLevel()) * 100
 ).PHP_EOL;
 
 // Imaginons que Greg l'emporte tout de même.
-Encounter::setNewLevel($greg, $jade->level, Encounter::RESULT_WINNER);
-Encounter::setNewLevel($jade, $greg->level, Encounter::RESULT_LOSER);
+Encounter::setNewLevel($greg, $jade->getLevel(), Encounter::RESULT_WINNER);
+Encounter::setNewLevel($jade, $greg->getLevel(), Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $greg->level,
-    $jade->level
+    $greg->getLevel(),
+    $jade->getLevel()
 );
 
 exit(0);
